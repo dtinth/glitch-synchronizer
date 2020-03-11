9e9079b661f44a3d8632922a702e2ab3b18be7c3
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-11 23:54:49 +0700

    Add voice-recorder project

---
+++ setup.js
@@ -0,0 +1,58 @@
+const { execSync } = require('child_process')
+const passthru = { stdio: 'inherit' }
+const projectName = process.argv[2]
+if (!projectName) {
+  throw new Error('No project name specified')
+}
+
+const ownerRepo = process.argv[3]
+if (!ownerRepo || ownerRepo.split('/').length !== 2) {
+  throw new Error('No owner/repo specified')
+}
+
+console.log('Glitch project name:', projectName)
+console.log('')
+
+const configPath = `projects/${projectName}.config.json`
+const privateKeyPath = `projects/${projectName}.sshkey`
+const publicKeyPath = `${privateKeyPath}.pub`
+const encryptionKeyPath = `projects/${projectName}.enckey`
+const encryptedEncryptionKeyPath = `${encryptionKeyPath}.enc`
+const encryptedPrivateKeyPath = `${privateKeyPath}.enc`
+
+// https://unix.stackexchange.com/questions/69314/automated-ssh-keygen-without-passphrase-how
+console.log('Generating a deploy keypair...')
+execSync(
+  `ssh-keygen -t rsa -f '${privateKeyPath}' -C 'glitch-synchronizer ${projectName} deploy key' -q -P ''`,
+  passthru,
+)
+
+// https://www.czeskis.com/random/openssl-encrypt-file.html
+console.log('')
+console.log('Generating a encryption key...')
+execSync(`openssl rand -base64 32 > '${encryptionKeyPath}'`, passthru)
+
+console.log('')
+console.log('Encrypting the encryption key...')
+execSync(
+  `openssl rsautl -encrypt -inkey keys/public.pem -pubin -in '${encryptionKeyPath}' -out '${encryptedEncryptionKeyPath}'`,
+  passthru,
+)
+
+console.log('')
+console.log('Encrypting the deploy key...')
+execSync(
+  `openssl enc -aes-256-cbc -salt -in '${privateKeyPath}' -out '${encryptedPrivateKeyPath}' -pass 'file:${encryptionKeyPath}'`,
+  passthru,
+)
+
+console.log('')
+console.log('Writing config...')
+require('fs').writeFileSync(
+  configPath,
+  JSON.stringify({ targetRepo: ownerRepo }, null, 2),
+)
+
+console.log('')
+console.log(`Keys generated. Add this deploy key to ${ownerRepo}:`)
+execSync(`cat '${publicKeyPath}'`, passthru)
